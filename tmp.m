a866d33794d1db27253d9c968d204dfac31f9491
Alou <als31@aber.ac.uk>
2014-05-10 17:08:22 +0100

    ras

---
+++ tmp.m
@@ -0,0 +1,15 @@
+Cd=imread('Will/Photometric_profile_left/linearized/07_C_d.tiff');
+X1d=imread('Will/Photometric_profile_left/linearized/01_X_d.tiff');
+Y1d=imread('Will/Photometric_profile_left/linearized/02_Y_d.tiff');
+Z1d=imread('Will/Photometric_profile_left/linearized/03_Z_d.tiff');
+X2d=imread('Will/Photometric_profile_left/linearized/04_X2_d.tiff');
+Y2d=imread('Will/Photometric_profile_left/linearized/05_Y2_d.tiff');
+Z2d=imread('Will/Photometric_profile_left/linearized/06_Z2_d.tiff');
+
+X1da=imread('Will/Photometric_profile_left/linearized/01_X_d_aligned.tiff');
+Y1da=imread('Will/Photometric_profile_left/linearized/02_Y_d_aligned.tiff');
+Z1da=imread('Will/Photometric_profile_left/linearized/03_Z_d_aligned.tiff');
+X2da=imread('Will/Photometric_profile_left/linearized/04_X2_d_aligned.tiff');
+Y2da=imread('Will/Photometric_profile_left/linearized/05_Y2_d_aligned.tiff');
+Z2da=imread('Will/Photometric_profile_left/linearized/06_Z2_d_aligned.tiff');
+
